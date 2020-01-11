49c8776820f86e60aeee6ed3e7114037d188892e
Edqe <34704796+Edqe14@users.noreply.github.com>
2020-01-11 20:26:27 +0700

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -4,6 +4,7 @@ const lyrics = require('./src/lyrics.js');
 module.exports = {
     search(query) {
         return new Promise((resolve, reject) => {
+            if(!query) return reject(new Error("Invalid Query"))
             search.search(query).then(dom => {
                 return resolve(search.extract(dom))
             }, rejection => reject(rejection));
@@ -11,6 +12,7 @@ module.exports = {
     },
     getLyric(query) {
         return new Promise((resolve, reject) => {
+            if(!query) return reject(new Error("Invalid Query"))
             search.search(query).then(dom => {
                 lyrics.reqLyric(search.extract(dom)[0]).then(dom => {
                     return resolve(lyrics.extractLyric(dom));
@@ -20,6 +22,7 @@ module.exports = {
     },
     getLyricFromLink(link) {
         return new Promise((resolve, reject) => {
+            if(!link) return reject(new Error("Invalid URI"))
             lyrics.reqLyric(link).then(dom => {
                 return resolve(lyrics.extractLyric(dom));
             }, rejection => reject(rejection))
